e1e2afdf13ecb4b6c34e4c63fe1cce616ce06a64
nareshsolanki99 <nareshsolanki3456@gmail.com>
2022-08-19 02:21:48 +0530

    folder structure

--- ReverseAnArray.js
+++
@@ -1,8 +0,0 @@
-// Reverse an Array of length N
-let arr = [2,4,5,7,9]
-for(let i=0;i<arr.length/2;i++){
-    let temp = arr[i]
-    arr[i] = arr[arr.length-1-i]
-    arr[arr.length-1-i] = temp
-}
-console.log(arr)
