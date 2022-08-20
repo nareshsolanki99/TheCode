5b2a043b828efd9e4d6205484482d1567c3b235c
nareshsolanki99 <nareshsolanki3456@gmail.com>
2022-08-20 18:30:19 +0530

    variables and datatypes

---
+++ BasicsOfProgramming/VariablesDatatypes.js
@@ -0,0 +1,18 @@
+let name = "Prashant"
+let age = 28
+let city = "Noida"
+let readyToRelocate = true
+let workExperience = null
+let certifications
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(readyToRelocate);
+console.log(workExperience);
+console.log(certifications);
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof city)
+console.log(typeof readyToRelocate)
+console.log(typeof workExperience)
+console.log(typeof certifications)
