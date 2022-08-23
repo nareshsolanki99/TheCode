2a3ce1fcf7bc027120148f1e137cfe7e36d715f0
nareshsolanki99 <nareshsolanki3456@gmail.com>
2022-08-23 19:03:09 +0530

    let var const

---
+++ BasicsOfProgramming/letconstvar.js
@@ -0,0 +1,38 @@
+// Declaration
+let age
+// Initialization
+age = 24
+// Declaration and Initialization
+let fName = "Dheeraj"
+
+//const
+{
+// console.log(a)
+const a = 25
+console.log(a)
+}
+//console.log(a)
+//a = 25
+
+
+//let 
+{
+//console.log(b)
+let b // Declare
+b = 25 // Initialize
+b = 26
+console.log(b)
+}
+// let b = 28
+// console.log(b)
+
+
+//var
+{
+console.log(c)
+var c // Declare
+c = 25 // Initialize
+var c = 27
+console.log(c)
+}
+console.log(c)
