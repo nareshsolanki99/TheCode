33bf592c4748cde2861829ec9e6d157eb7fadfe3
nareshsolanki99 <nareshsolanki3456@gmail.com>
2022-08-28 04:16:08 +0530

    operators

---
+++ BasicsOfProgramming/operators.js
@@ -0,0 +1,62 @@
+let age = 25 // Assignment
+//a b
+//Test1
+// a = 76
+// b = 65
+//Test2
+// a = 64
+// b = 82
+const aTest1 = 76
+const bTest1 = 64
+const aTest2 = 71
+const bTest2 = 82
+const aTotal = aTest1 + aTest2
+const bTotal = bTest1 + bTest2
+const totalMarks = 200
+console.log(aTotal)
+console.log(bTotal)
+console.log(aTotal/totalMarks*100)
+console.log(bTotal/totalMarks*100)
+const diff = bTotal - aTotal
+console.log(diff)
+//Comparison
+console.log(aTotal > bTotal)
+console.log(aTotal < bTotal)
+console.log(aTotal >= bTotal)
+console.log(aTotal <= bTotal)
+console.log(aTotal == bTotal)
+//Exponential
+console.log(5**5)
+//Modulus Remainder
+// 16 /4 
+// Dividend => 16
+// Divisor => 4
+// Quotient => 4
+// Remainder => 0
+console.log(16%4)
+// Increment && Decrement
+let count = 1
+//count = count + 1
+count++
+count++
+count++
+// count = count + 1
+console.log(count++)
+console.log(count)
+console.log(count++)
+// count = count - 1
+console.log(count--)
+console.log(count)
+console.log(--count)
+
+//Addition assignment
+let ticket = 500
+//ticket = ticket + 10
+//ticket = ticket + 10
+ticket += 10
+// ticket = ticket - 10
+ticket -= 10
+// ticket = ticket * 10
+ticket *= 10
+
+console.log(ticket)
